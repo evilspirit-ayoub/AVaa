bc46e637076981a96d8cb3891a4cc7e3ccc4b658
Zhetsu <Alkarose@hotmail.fr>
2022-06-07 14:02:39 +0200

    z

--- lib/src/main/java/DSBot/Library.java
+++ lib/src/main/java/DSBot/Library.java
@@ -21,7 +21,7 @@ import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class Library {
-	
+
 	private final EventWaiter eventWaiter = new EventWaiter();
 	
     public static void main(String ... args) throws Exception {
